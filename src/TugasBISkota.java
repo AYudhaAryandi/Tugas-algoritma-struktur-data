c17e20aee0c2936583a599b12426ae88bf71a353
LENOVO <yudhaaryandi03@gmail.com>
2022-12-24 08:41:15 +0700

    Tugas_Algoritma_Struktur-Data.iml

---
+++ src/TugasBISkota.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class TugasBISkota {
+    public static void main(String[] args) {
+        ArrayList<int[]> BisKota = new ArrayList<int[]>();
+        BisKota.add(new int[]{10, 0});
+        BisKota.add(new int[]{3, 5});
+        BisKota.add(new int[]{2, 5});
+        System.out.println(GetPassenger(BisKota));
+    }
+    public static int GetPassenger(ArrayList<int[]> BisKota) {
+        return 5;
+    }
+}
+
+
+
